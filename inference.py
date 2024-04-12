b992fceaa982c9a5f209ed4175dd6fbee3bbef74
Patryk Ploszaj <patryk.ploszaj07@gmail.com>
2024-04-12 12:33:09 -0400

    implement constructBayesNet function

--- inference.py
+++ inference.py
@@ -56,12 +56,23 @@ def constructBayesNet(gameState: hunters.GameState):
     Y_RANGE = gameState.getWalls().height
     MAX_NOISE = 7
 
-    variables = []
-    edges = []
+    variables = [PAC, GHOST0, GHOST1, OBS0, OBS1]
+    edges = [(GHOST0, OBS0), (GHOST1, OBS1), (PAC, OBS0), (PAC, OBS1)]
     variableDomainsDict = {}
 
-    "*** YOUR CODE HERE ***"
-    raiseNotDefined()
+    positions = [(x, y) for x in range(X_RANGE) for y in range(Y_RANGE)]
+    variableDomainsDict[PAC] = positions
+    variableDomainsDict[GHOST0] = positions
+    variableDomainsDict[GHOST1] = positions
+    
+    max_distance = (X_RANGE - 1) + (Y_RANGE - 1)
+    max_obs_value = max_distance + MAX_NOISE
+
+    observations = list(range(max_obs_value + 1))
+    variableDomainsDict[OBS0] = observations
+    variableDomainsDict[OBS1] = observations
+    
+    # raiseNotDefined()
     "*** END YOUR CODE HERE ***"
 
     net = bn.constructEmptyBayesNet(variables, edges, variableDomainsDict)
